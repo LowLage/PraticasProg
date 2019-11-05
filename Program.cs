ddf75e7df60dbad7e6a9ddff0a3f17f1bf4a7c76
Lourenço <loulage@gmail.com>
2019-11-05 11:23:28 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace L04E09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
